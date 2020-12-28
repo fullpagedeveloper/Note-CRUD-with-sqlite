293ef11b1a43c456ad1a42bbff15d8153b0a1207
MaulanaWest <maulana2ahmad@gmail.com>
2020-12-29 00:17:39 +0700

    add comment

--- app/src/main/java/com/fullpagedeveloper/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/fullpagedeveloper/mynotesapp/MainActivity.kt
@@ -93,14 +93,20 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (data != null) {
             when (requestCode) {
+                // Akan dipanggil jika request codenya ADD
                 NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
                     val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
                     adapter.addItem(note)
                     binding.rvNotes.smoothScrollToPosition(adapter.itemCount - 1)
                     showSnackbarMessage("Satu item berhasil ditambahkan")
                 }
+                // Update dan Delete memiliki request code sama akan tetapi result codenya berbeda
                 NoteAddUpdateActivity.REQUEST_UPDATE ->
                     when (resultCode) {
+                        /**
+                        Akan dipanggil jika result codenya  UPDATE
+                        Semua data di load kembali dari awal
+                        */
                         NoteAddUpdateActivity.RESULT_UPDATE -> {
                             val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
                             val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
@@ -108,6 +114,10 @@ class MainActivity : AppCompatActivity() {
                             binding.rvNotes.smoothScrollToPosition(position)
                             showSnackbarMessage("Satu item berhasil diubah")
                         }
+                        /**
+                        Akan dipanggil jika result codenya DELETE
+                        Delete akan menghapus data dari list berdasarkan dari position
+                        */
                         NoteAddUpdateActivity.RESULT_DELETE -> {
                             val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
                             adapter.removeItem(position)
